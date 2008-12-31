094111e696f1687821964b5cd3b52cbc7ac55c0f
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2008-12-31 20:01:20 +0000

    Copied over unit tests for Person model	from cq_rails project.

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -1,8 +1,37 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :people
+
+  def test_acts_as_versioned
+    p = Person.new
+    assert_nil p.version
+    p.last_name = 'Foo'
+    assert p.save!
+    assert p.version == 1
+    assert p.save!    #  Verify that versions_conditions_met? does not fail
   end
+
+  def test_should_validate_with_last_name
+    p = Person.new
+    p.last_name = 'last_name'
+    assert_equal p.valid?, true
+  end
+
+  def test_should_not_validate_with_empty_last_name
+    p = Person.new
+    p.last_name = ''
+    assert_equal p.valid?, false
+  end
+  
+# def test_should_create_entity
+#  p = Person.new
+#  p.last_name = 'Foo'
+#  assert_nil p.entity
+#  assert_not_nil p.save!
+#  p.reload
+#  assert_not_nil p.entity
+#  assert_equal p, p.entity.resource
+# end
+  
 end
