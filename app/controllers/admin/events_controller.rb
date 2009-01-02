19eba7b36e9f2564c12f1fb561b111c91c3ab027
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2009-01-02 23:55:11 +0000

    Created	admin/events/list page.

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,2 +1,11 @@
 class Admin::EventsController < ApplicationController
+
+  def index
+    @total_events = ContactEvent.count
+  end
+
+  def list
+    @events = ContactEvent.find(:all)
+  end
+
 end
--- test/functional/admin/events_controller_test.rb
+++ test/functional/admin/events_controller_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class Admin::EventsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  fixtures :contact_events
+  
+
+  #  Test 'index' action
+
+  def test_should_allow_admin_access_to_index
+    get_page_as_admin_with_no_errors(:index)
   end
+
+
+  #  Test 'list' action
+
+  def test_should_allow_admin_access_to_list
+    get_page_as_admin_with_no_errors(:list)
+  end
+
 end
