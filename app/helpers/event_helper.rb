d7e90bcb7a87a42dc21680b4335df205a1d36b8a
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2009-01-02 18:05:33 +0000

    Created	event helper function for formatting the start/end date	range.

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,2 +1,19 @@
 module EventHelper
+
+  def format_event_date_range(e)
+    #  lemma: e.start_date <= e.end_date because of ContactEvent.before_save
+    if e.start_date.year != e.end_date.year
+      "#{e.start_date.strftime('%b %e, %Y')} - #{e.end_date.strftime('%b %e, %Y')}"
+    elsif e.start_date.month != e.end_date.month
+      "#{e.start_date.strftime('%b %e')} - #{e.end_date.strftime('%b %e, %Y')}"
+    elsif e.start_date.day != e.end_date.day
+      "#{e.start_date.strftime('%b %e')} - #{e.end_date.strftime('%e, %Y')}"
+    else
+      e.start_date.strftime('%b %e, %Y')
+    end
+  end
+
+  def format_event_location(l)
+  end
+
 end
