c10ca715e9c92f64f18b6096a026e1eb4aa5337a
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2009-01-18 21:46:18 +0000

    Events listed on the home page are now sorted by start date, as opposed to the order they were entered into the database.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     require 'RedCloth'
-    @events = EventEntry.find(:all)
+    @events = EventEntry.find(:all, :order => 'start_date ASC')
   end
 
 end
