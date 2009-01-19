ec9c3e05302753e67b54ba2dd531e862f1d686c5
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2009-01-19 18:15:30 +0000

    Added restart_rails script, which fixes the permissions on config/environment.rb so that the Rails application comes back up properly.

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -72,7 +72,7 @@ CREATE TABLE `ci_email_versions` (
   KEY `index_ci_email_versions_on_email_id` (`email_id`),
   KEY `index_ci_email_versions_on_for_entity_id` (`for_entity_id`),
   KEY `index_ci_email_versions_on_address` (`address`)
-) ENGINE=MyISAM AUTO_INCREMENT=15002 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15009 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_emails` (
   `email_id` int(11) NOT NULL auto_increment,
@@ -85,7 +85,7 @@ CREATE TABLE `ci_emails` (
   PRIMARY KEY  (`email_id`),
   KEY `index_ci_emails_on_for_entity_id` (`for_entity_id`),
   KEY `index_ci_emails_on_address` (`address`)
-) ENGINE=MyISAM AUTO_INCREMENT=15002 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15009 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_event_entries` (
   `event_entry_id` int(11) NOT NULL auto_increment,
@@ -107,7 +107,7 @@ CREATE TABLE `ci_event_entries` (
   `created_at` datetime default NULL,
   `updated_at` datetime default NULL,
   PRIMARY KEY  (`event_entry_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15003 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15002 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_event_entry_versions` (
   `event_entry_version_id` int(11) NOT NULL auto_increment,
@@ -129,7 +129,7 @@ CREATE TABLE `ci_event_entry_versions` (
   `ci_notes` text,
   `updated_at` datetime default NULL,
   PRIMARY KEY  (`event_entry_version_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15005 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15002 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_jam_entries` (
   `jam_entry_id` int(11) NOT NULL auto_increment,
@@ -190,7 +190,7 @@ CREATE TABLE `ci_location_versions` (
   KEY `index_ci_location_versions_on_location_id` (`location_id`),
   KEY `index_ci_location_versions_on_us_state_id` (`us_state_id`),
   KEY `index_ci_location_versions_on_country_name_id` (`country_name_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15005 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15002 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_locations` (
   `location_id` int(11) NOT NULL auto_increment,
@@ -210,7 +210,7 @@ CREATE TABLE `ci_locations` (
   PRIMARY KEY  (`location_id`),
   KEY `index_ci_locations_on_us_state_id` (`us_state_id`),
   KEY `index_ci_locations_on_country_name_id` (`country_name_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15003 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15002 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_organization_entries` (
   `organization_entry_id` int(11) NOT NULL auto_increment,
@@ -276,7 +276,7 @@ CREATE TABLE `ci_people` (
   `created_at` datetime default NULL,
   `updated_at` datetime default NULL,
   PRIMARY KEY  (`person_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15004 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15012 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_person_entries` (
   `person_entry_id` int(11) NOT NULL auto_increment,
@@ -322,7 +322,7 @@ CREATE TABLE `ci_person_versions` (
   `last_name` varchar(255) default NULL,
   `updated_at` datetime default NULL,
   PRIMARY KEY  (`person_version_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15004 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15012 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_phone_number_versions` (
   `phone_number_version_id` int(11) NOT NULL auto_increment,
@@ -334,7 +334,7 @@ CREATE TABLE `ci_phone_number_versions` (
   PRIMARY KEY  (`phone_number_version_id`),
   KEY `index_ci_phone_number_versions_on_phone_number_id` (`phone_number_id`),
   KEY `index_ci_phone_number_versions_on_for_entity_id` (`for_entity_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15001 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15002 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_phone_numbers` (
   `phone_number_id` int(11) NOT NULL auto_increment,
@@ -345,7 +345,7 @@ CREATE TABLE `ci_phone_numbers` (
   `updated_at` datetime default NULL,
   PRIMARY KEY  (`phone_number_id`),
   KEY `index_ci_phone_numbers_on_for_entity_id` (`for_entity_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15001 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15002 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_schema_migrations` (
   `version` varchar(255) NOT NULL,
@@ -447,7 +447,7 @@ CREATE TABLE `ci_user_account_requests` (
   `created_at` datetime default NULL,
   `updated_at` datetime default NULL,
   PRIMARY KEY  (`user_account_request_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15001 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15007 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ci_users` (
   `user_id` int(11) NOT NULL auto_increment,
@@ -473,7 +473,7 @@ CREATE TABLE `ci_users` (
   KEY `index_ci_users_on_state` (`state`),
   KEY `index_ci_users_on_password_reset_code` (`password_reset_code`),
   KEY `index_ci_users_on_activation_code` (`activation_code`)
-) ENGINE=MyISAM AUTO_INCREMENT=15002 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15007 DEFAULT CHARSET=utf8;
 
 INSERT INTO ci_schema_migrations (version) VALUES ('20081216170645');
 
