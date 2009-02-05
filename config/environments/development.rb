0389439d48f755d2e76f0e8d3e63719bb0ac8861
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2009-02-05 16:11:57 +0000

    The entries/jam	partial	template was only displaying the description field for jams,	and not	the cost or schedule fields.
    Tweaked CSS spacing between description, cost and schedule fields.
    
    Added HTML describing somte Textile commands to the form_fields
    partials for Jam, Person and Organization entries.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,9 +11,9 @@ config.whiny_nils = true
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
-# config.action_controller.perform_caching             = false
+config.action_controller.perform_caching             = false
 # [CTH]  Enable caching for development environment
-config.action_controller.perform_caching             = true
+#config.action_controller.perform_caching             = true
 # [/CTH]
 
 # Don't care if the mailer can't send
