2cbd3ae919eda0a3ce35c4690ed6902ac15a9a99
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2009-09-27 23:35:44 +0000

    Added EventEntry.distinct_years()	and EventEntry.distinct_months(year) functions.  Did	some refactoring of '/events/list' in preparation for grouping events	on that	page by	year and month,	instead	of just	one long sorted chronological list.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,11 +12,28 @@ class EventsController < ApplicationController
 
 
   def list
-    if params[:year] and params[:month]
-      @entries = EventEntry.find_by_year_month(params[:year], params[:month])
-    elsif params[:year]
-      @entries = EventEntry.find_by_year(params[:year])
+    year = params[:year]
+    month = params[:month]
+
+    #  If we are listing events for a specific year and month, then we show events whose start date
+    #   OR end date is in that month [using EventEntry.find_by_year_month()];
+    #  Otherwise, when displaying multiple months, for a given month we only list events whose start
+    #   date is in that month [using EventEntry.find_by_start_date_year_month()].  This prevents an
+    #   event that spans two months from being listed twice.
+    if year and month
+      @entries_by_year_month = { year => { month => EventEntry.find_by_year_month(year, month) } }
+      @entries = EventEntry.find_by_year_month(year, month)
+    elsif year
+      @entries_by_year_month = Hash.new
+      @entries_by_year_month[year] = Hash.new
+      EventEntry.distinct_months(year).each { |m| @entries_by_year_month[year][m] = EventEntry.find_by_start_date_year_month(year, m) }
+      @entries = EventEntry.find_by_year(year)
     else
+      @entries_by_year_month = Hash.new
+      EventEntry.distinct_years.each do |y| 
+        @entries_by_year_month[y] = Hash.new
+        EventEntry.distinct_months(y).each { |m| @entries_by_year_month[y][m] = EventEntry.find_by_start_date_year_month(y, m) }
+      end
       @entries = EventEntry.find(:all, :order => 'start_date ASC')
     end
   end
--- app/models/event_entry.rb
+++ app/models/event_entry.rb
@@ -16,25 +16,44 @@ class EventEntry < ActiveRecord::Base
   validates_presence_of :title, :description, :start_date, :end_date
 
 
-  def self.find_by_year(year)
+  def self.distinct_months(year)
+    EventEntry.find_by_sql("SELECT DISTINCT DATE_FORMAT(start_date, '%Y') " + 
+      "AS year, DATE_FORMAT(start_date, '%m') AS month FROM ci_event_entries " + 
+      "WHERE DATE_FORMAT(start_date, '%Y') = '#{year}' ORDER BY year, month;").collect { |e| e.month }
+  end
+
+  def self.distinct_years
+    EventEntry.find_by_sql("SELECT DISTINCT DATE_FORMAT(start_date, '%Y') " + 
+      "AS year FROM ci_event_entries ORDER BY year;").collect { |e| e.year }
+  end
+
+  def self.find_by_year(year, by_start_date_only = false)
     first_of_year = "'#{year}-01-01'"
     last_of_year  = "'#{year}-12-31'"
     @entries = EventEntry.find(:all, :order => 'start_date ASC',
-      :conditions => "(start_date >= #{first_of_year} AND start_date <= #{last_of_year}) OR " +
-                     "(end_date >= #{first_of_year} AND end_date <= #{last_of_year})"
+      :conditions => "(start_date >= #{first_of_year} AND start_date <= #{last_of_year}) " +
+                     (by_start_date_only ? '' : "OR (end_date >= #{first_of_year} AND end_date <= #{last_of_year})")
     )
   end
   
-  def self.find_by_year_month(year, month)
+  def self.find_by_start_date_year(year)
+    self.find_by_year(year, true)
+  end
+  
+  def self.find_by_year_month(year, month, by_start_date_only = false)
     first_of_month = "'#{year}-#{month}-01'"
     last_of_month  = "'#{year}-#{month}-31'"
     EventEntry.find(:all, :order => 'start_date ASC', 
-      :conditions => "(start_date >= #{first_of_month} AND start_date <= #{last_of_month}) OR " +
-                     "(end_date >= #{first_of_month} AND end_date <= #{last_of_month}) "
+      :conditions => "(start_date >= #{first_of_month} AND start_date <= #{last_of_month}) " +
+                     (by_start_date_only ? '' : "OR (end_date >= #{first_of_month} AND end_date <= #{last_of_month}) ")
                      #  Line below commented out because all current events that last longer
                      #   than a month are actually misclassified jams
                      # "(start_date < #{first_of_month} AND end_date > #{last_of_month})" 
     )
+  end
+
+  def self.find_by_start_date_year_month(year, month)
+    self.find_by_year_month(year, month, true)
   end
 
 
