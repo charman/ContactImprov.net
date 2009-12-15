5aae7ff017b963e85781f1020d2dd000da642588
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2009-12-15 04:27:15 +0000

    Got tabbed infowindows working with Google Maps	v3 and jQuery.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -23,7 +23,7 @@ protected
 
   def marker_info_for_entry(entry)
     marker_info = Hash.new
-    marker_info['class'] = entry.class.to_s
+    marker_info['entry_class'] = entry.class.to_s.sub(/Entry/,'')
     marker_info['mouseover_text'] = entry.title
     marker_info['infowindow_html'] = infowindow_for_entry(entry)
     marker_info
@@ -31,13 +31,13 @@ protected
 
   def infowindow_for_entry(entry)
     case entry.class.to_s
-    when "EventEntry"
+    when /Event/
       infowindow_for_event(entry)
-    when "JamEntry"
+    when /Jam/
       infowindow_for_jam(entry)
-    when "OrganizationEntry"
+    when /Organization/
       infowindow_for_organization(entry)
-    when "PersonEntry"
+    when /Person/
       infowindow_for_person(entry)
     end
   end
@@ -45,7 +45,7 @@ protected
   def infowindow_for_event(event)
     "<div style=\"font-size: 1.1em; font-weight: bold;\">" + 
       "<a href=\"/events/show/#{event.id}\">#{event.title}</a></div>" +
-      "<div>#{format_event_date_range(event)}</div> " +
+      "<div>#{event.date_range}</div> " +
       (event.url ? "<div style=\"font-size: 0.9em;\"><a href=\"#{event.url.address}\">#{event.url.address}</a></div>" : '')
   end
 
@@ -65,21 +65,6 @@ protected
     "<div style=\"font-size: 1.1em; font-weight: bold;\">" + 
       "<a href=\"/people/show/#{person.id}\">#{person.title}</a></div>" +
       (person.url ? "<div style=\"font-size: 0.9em;\"><a href=\"#{person.url.address}\">#{person.url.address}</a></div>" : '')
-  end
-
-
-  ## TODO: Copied from helpers/event_helper.rb; refactor to avoid code duplication
-  def format_event_date_range(e)
-    #  lemma: e.start_date <= e.end_date because of EventEntry.before_save
-    if e.start_date.year != e.end_date.year
-      "#{e.start_date.strftime('%b %e, %Y')} - #{e.end_date.strftime('%b %e, %Y')}"
-    elsif e.start_date.month != e.end_date.month
-      "#{e.start_date.strftime('%b %e')} - #{e.end_date.strftime('%b %e, %Y')}"
-    elsif e.start_date.day != e.end_date.day
-      "#{e.start_date.strftime('%b %e')} - #{e.end_date.strftime('%e, %Y')}"
-    else
-      e.start_date.strftime('%b %e, %Y')
-    end
   end
 
 end
--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -1,2 +1,12 @@
 module MapHelper
+  
+  def javascript_array_for_tabbed_infowindow(markerinfo)
+    ja = markerinfo.collect { |info|
+      "{mouseover_text:'#{info['mouseover_text']}', " + 
+      "infowindow_html:'#{info['infowindow_html']}', " + 
+      "entry_class:'#{info['entry_class']}'}"
+    }.join(',')
+    "[#{ja}]".gsub(/\"/, '\"')
+  end
+  
 end
