d37c42f88e3462f852bd614d3567764a4d8bac2a
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2011-05-27 21:31:03 +0000

    Added configuration files necessary for running Rails 3 server.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CiRail::Application.config.secret_token = '42e444a2a184bda8f6d931ce08cbb232df5771decb6139cfeabb5a808cc99372006f152a3638fc3c9ebd3ccd3b9525ec9071761f5022d641ae08ee908c419fd0'
