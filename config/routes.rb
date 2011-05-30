d13db2746c178defb5b28af15282ffb7c4fa6d88
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2011-05-30 16:59:46 +0000

    Fixed the (Rails 3) routes file so that the admin pages now work again.

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,26 @@ ContactImprovNet::Application.routes.draw do
   match '/reset_password/:password_reset_code' => 'user#reset_password', :as => :activate
   match '/login' => 'sessions#new', :as => :login
   match '/logout' => 'sessions#destroy', :as => :logout
+
   match '/' => 'home#index', :as => :index
-  match '/admin' => 'admin/home#index', :as => :admin_index
+
+  namespace 'admin' do
+    match '/' => 'home#index'
+    match '/account_requests' => 'account_requests#index'
+    match '/events' => 'events#index'
+    match '/geocoding' => 'geocoding#index'
+    match '/jams' => 'jams#index'
+    match '/motivation' => 'motivation#index'
+    match '/organizations' => 'organizations#index'
+    match '/people' => 'people#index'
+    match '/users' => 'users#index'
+  end
+
   match '/events' => 'events#index', :as => :events_index
   match '/jams' => 'jams#index', :as => :jams_index
   match '/people' => 'people#index', :as => :people_index
   match '/organizations' => 'organizations#index', :as => :organizations_index
+
   match '/events/list/:year/:month' => 'events#list'
   match '/events/list/:year' => 'events#list'
   match '/jams/list/:country_name/:us_state' => 'jams#list'
@@ -25,8 +39,11 @@ ContactImprovNet::Application.routes.draw do
   match '/organizations/list/:country_name' => 'organizations#list'
   match '/people/list/:country_name/:us_state' => 'people#list'
   match '/people/list/:country_name' => 'people#list'
+
   match '/events/calendar.ics' => 'events#calendar'
   match '/events/:country_name/calendar.ics' => 'events#calendar'
   match '/events/:country_name/:us_state/calendar.ics' => 'events#calendar'
+
+  match ':controller(/:action(/:id))', :controller => /admin\/[^\/]+/
   match '/:controller(/:action(/:id))'
 end
