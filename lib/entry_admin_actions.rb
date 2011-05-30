f4b0c9172de4bdd0138984f25e62ebb76f9e4610
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2011-05-30 17:09:22 +0000

    Fixed a problem where the layout wasn't being displayed for the admin pages for the different types of entries (Events, People, etc), because a template was being rendered as a partial instead of as a full page.

--- lib/entry_admin_actions.rb
+++ lib/entry_admin_actions.rb
@@ -2,12 +2,12 @@ module EntryAdminActions
   
   def index
     @total_entries = entry_class.count
-    render :partial => "shared/admin/entries/index", :locals => { :entry_display_name => entry_display_name }
+    render "shared/admin/entries/index", :locals => { :entry_display_name => entry_display_name }
   end
   
   def list
     @entries = entry_class.find(:all)
-    render :partial => "shared/admin/entries/list", :locals => { :entry_display_name => entry_display_name }
+    render "shared/admin/entries/list", :locals => { :entry_display_name => entry_display_name }
   end
   
 end
