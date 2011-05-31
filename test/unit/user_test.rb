a5e0313da165acdcdc10caee6a4f44e1fc8872cd
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2011-05-31 21:33:38 +0000

    Cleaned up (commented out) vestigial unit tests related to restful_authentication.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,14 +2,6 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  def setup
-    @emails     = ActionMailer::Base.deliveries
-    @emails.clear
-
-    activate_authlogic
-  end
-
-
   def test_should_create_user
     assert_difference 'User.count' do
       create_user(:email => 'createuser@contactimprov.org')
@@ -88,53 +80,9 @@ class UserTest < ActiveSupport::TestCase
     assert_equal users(:quentin), User.authenticate('quentin@contactimprov.org', 'new password')
   end
 
-# def test_should_not_rehash_password
-#   users(:quentin).update_attributes(:email => 'quentin2@contactimprov.org')
-#   assert_equal users(:quentin), User.authenticate('quentin2@contactimprov.org', 'test')
-# end
-
   def test_should_authenticate_user
     assert_equal users(:quentin), User.authenticate('quentin@contactimprov.org', 'test')
   end
-
-# def test_should_set_remember_token
-#   users(:quentin).remember_me
-#   assert_not_nil users(:quentin).remember_token
-#   assert_not_nil users(:quentin).remember_token_expires_at
-# end
-# 
-# def test_should_unset_remember_token
-#   users(:quentin).remember_me
-#   assert_not_nil users(:quentin).remember_token
-#   users(:quentin).forget_me
-#   assert_nil users(:quentin).remember_token
-# end
-# 
-# def test_should_remember_me_for_one_week
-#   before = 1.week.from_now.utc
-#   users(:quentin).remember_me_for 1.week
-#   after = 1.week.from_now.utc
-#   assert_not_nil users(:quentin).remember_token
-#   assert_not_nil users(:quentin).remember_token_expires_at
-#   assert users(:quentin).remember_token_expires_at.between?(before, after)
-# end
-# 
-# def test_should_remember_me_until_one_week
-#   time = 1.week.from_now.utc
-#   users(:quentin).remember_me_until time
-#   assert_not_nil users(:quentin).remember_token
-#   assert_not_nil users(:quentin).remember_token_expires_at
-#   assert_equal users(:quentin).remember_token_expires_at, time
-# end
-# 
-# def test_should_remember_me_default_two_weeks
-#   before = 2.weeks.from_now.utc
-#   users(:quentin).remember_me
-#   after = 2.weeks.from_now.utc
-#   assert_not_nil users(:quentin).remember_token
-#   assert_not_nil users(:quentin).remember_token_expires_at
-#   assert users(:quentin).remember_token_expires_at.between?(before, after)
-# end
 
   def test_should_register_passive_user
     assert users(:passive_user).passive?
@@ -184,7 +132,9 @@ class UserTest < ActiveSupport::TestCase
   end
 
 protected
+
   def create_user(options = {})
     User.create({ :email => 'quire@example.com', :password => 'quired', :password_confirmation => 'quired' }.merge(options))
   end
+
 end
