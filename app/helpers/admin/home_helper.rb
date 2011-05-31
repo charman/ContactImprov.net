a6523ff575c7e69239727b4846c302cc97877cfc
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2011-05-31 13:49:24 +0000

    Fixed warning message caused by having a space before the opening paren of a function.

--- app/helpers/admin/home_helper.rb
+++ app/helpers/admin/home_helper.rb
@@ -1,7 +1,7 @@
 module Admin::HomeHelper
 
   def admin_controller_breadcrumb_link
-    link_to (params[:controller].gsub('admin/', '').titleize.gsub('_', ''), "/#{params[:controller]}") 
+    link_to params[:controller].gsub('admin/', '').titleize.gsub('_', ''), "/#{params[:controller]}"
   end
 
 end
