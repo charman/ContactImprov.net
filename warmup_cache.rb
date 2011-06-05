f5bf923a58ea2a40e9c59d8d1806e8972ba10383
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2011-06-05 01:08:09 +0000

    Created script for warming up cache.

---
+++ warmup_cache.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+require 'net/http'
+
+hostname = (ARGV[0] ? ARGV[0]      : 'localhost')
+port     = (ARGV[1] ? ARGV[1].to_i : 3000)
+
+print "hostname = #{hostname}\n"
+print "port     = #{port}\n\n"
+
+
+paths = [
+  '/',
+  '/events',
+  '/events/list',
+  '/jams',
+  '/jams/list',
+  '/people',
+  '/people/list',
+  '/organizations',
+  '/organizations/list'
+]
+
+paths.each do |path|
+  before_cold = Time.now
+  Net::HTTP.get hostname, path, port
+  dt_cold = Time.now - before_cold
+  print "COLD: %.3f - #{path}\n" % dt_cold
+
+  before_warm = Time.now
+  Net::HTTP.get hostname, path, port
+  dt_warm = Time.now - before_warm
+  print "WARM: %.3f - #{path}\n\n" % dt_warm
+end
