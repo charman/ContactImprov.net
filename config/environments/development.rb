429b42c8f46498928dbc53994c6c51c5acbf61be
charman <charman@79eeaa25-2c90-4b01-8359-b2079eff6bc7>
2011-11-08 15:25:11 +0000

    While upgrading to Rails 3.1, I was receiving the error message:
      /Library/Ruby/Gems/1.8/gems/actionpack-3.1.1/lib/action_view/railtie.rb:34:in `send': undefined method `debug_rjs=' for ActionView::Base:Class (NoMethodError)
    
    which was caused by this line in config/environments/development.rb:
    
      config.action_view.debug_rjs = true
    
    The 'debug_rjs=' function has apparently been deprecated:
    
      https://github.com/rails/rails/issues/2098
    
    Removing the call fixed the problem.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,6 @@ ContactImprovNet::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local = true
-  config.action_view.debug_rjs                         = true
 #  config.action_controller.perform_caching             = false
   config.action_controller.perform_caching             = true
 
